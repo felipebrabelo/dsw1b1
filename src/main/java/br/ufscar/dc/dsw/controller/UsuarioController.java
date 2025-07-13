e7ffc46e8a68841b63414e2ff40b064ed9fed3b0
devfabri <faguezpersonal@gmail.com>
2025-07-13 15:50:13 -0300

    feat: candidatura com curriculo

--- src/main/java/br/ufscar/dc/dsw/controller/UsuarioController.java
+++ src/main/java/br/ufscar/dc/dsw/controller/UsuarioController.java
@@ -1,27 +1,17 @@
 package br.ufscar.dc.dsw.controller;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.ufscar.dc.dsw.dao.ICandidaturaDAO;
-import br.ufscar.dc.dsw.dao.IVagaDAO;
-import br.ufscar.dc.dsw.domain.Candidatura;
 import br.ufscar.dc.dsw.domain.Empresa;
 import br.ufscar.dc.dsw.domain.Profissional;
-import br.ufscar.dc.dsw.domain.Vaga;
-import br.ufscar.dc.dsw.service.impl.CandidaturaService;
 import br.ufscar.dc.dsw.service.impl.EmpresaService;
 import br.ufscar.dc.dsw.service.impl.ProfissionalService;
 import jakarta.validation.Valid;
@@ -39,36 +29,53 @@ public class UsuarioController {
 	private BCryptPasswordEncoder encoder;
 
   @GetMapping("/cadastrar")
-  public String cadastrarProfissional(Profissional profissional, ModelMap model) {
+  public String cadastrarProfissional(ModelMap model) {
     model.addAttribute("type", "PROFISSIONAL");
+    model.addAttribute("usuario", new Profissional());
     return "usuario/cadastro";
   }
 
-  @GetMapping("/cadastrar-empresa")
-  public String cadastraEmpresa(Empresa empresa, ModelMap model) {
-    model.addAttribute("action", "/salvar-empresa");
-    return "empresa/cadastro";
+  @GetMapping("/cadastrar/empresa")
+  public String cadastraEmpresa(ModelMap model) {
+    model.addAttribute("type", "EMPRESA");
+    model.addAttribute("usuario", new Empresa());
+    return "usuario/cadastro";
   }
 
   @PostMapping("/salvar")
-  public String salvaEmpresa(@Valid Profissional profissional, BindingResult result, RedirectAttributes attr) {
+  public String salvaProfissional(@Valid Profissional profissional, BindingResult result, RedirectAttributes attr) {
+    System.out.println("Salvando profissional: " + profissional);
     if (result.hasErrors()) {
       return "profissional/cadastro";
     }
 
     profissional.setSenha(encoder.encode(profissional.getSenha()));
     profissionalService.salvar(profissional);
-    attr.addFlashAttribute("success", "Usuario criado com sucesso");
+
+    System.out.println("Profissional salvo: " + profissional);
+
+    attr.addFlashAttribute("success", "usuario.create.success");
+    return "redirect:/login";
+  }
+
+  @PostMapping("/salvar/empresa")
+  public String salvaEmpresa(@Valid Empresa empresa, BindingResult result, RedirectAttributes attr) {
+    System.out.println("Salvando empresa: " + empresa);
+    if (result.hasErrors()) {
+      return "empresa/cadastro";
+    }
+
+    empresa.setSenha(encoder.encode(empresa.getSenha()));
+    empresaService.salvar(empresa);
+
+    System.out.println("Empresa salva: " + empresa);
+
+    attr.addFlashAttribute("success", "usuario.create.success");
     return "redirect:/login";
   }
 
   @GetMapping("/usuarios")
   public String listUsuarios(@RequestParam String cargo) {
     return "usuario/list";
-  }
-
-  @GetMapping("/usuarios/{id}")
-  public String detailUsuario(@PathVariable("id") Long id, ModelMap model) {
-    return "usuario/details";
   }
 }
